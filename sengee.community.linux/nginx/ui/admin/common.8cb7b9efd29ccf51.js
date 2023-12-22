9c04a99b780f7770e5b5b8a6fb71b438efa74886
suhe04551 <hehunan2009@163.com>
2023-12-22 10:57:18 +0800

    删除旧页面

--- sengee.community.linux/nginx/ui/admin/common.8cb7b9efd29ccf51.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkadmin=self.webpackChunkadmin||[]).push([[592],{3827:(e,E,_)=>{_.d(E,{x:()=>O});var a=_(9808),r=_(685),t=_(9445),M=_(8452),d=_(5156),o=_(5e3);let O=(()=>{class n{}return n.\u0275fac=function(P){return new(P||n)},n.\u0275mod=o.oAB({type:n}),n.\u0275inj=o.cJS({imports:[[a.ez,r.Xo,t.YI,M.vO,d.QX]]}),n})()}}]);
